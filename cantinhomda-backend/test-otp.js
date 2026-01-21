328def947747abdc2bf4513f8afbda42093d5ac3
Alex Seabra <aseabra2005@gmail.com>
2026-01-21 13:44:26 -0300

    chore: remove test files

--- cantinhomda-backend/test-otp.js
+++
@@ -1,9 +0,0 @@
-
-try {
-    const { authenticator } = require('otplib');
-    console.log('Require success');
-    const secret = authenticator.generateSecret();
-    console.log('Secret:', secret);
-} catch (e) {
-    console.error('Error:', e);
-}
